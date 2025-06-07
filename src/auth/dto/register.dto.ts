46f7ce40bac952f015694d8bcb6bed11c84c88a9
Joaquin <jetchegaray@opensa.com.ar>
2025-06-07 11:19:03 -0300

    revision sprint 1

--- src/auth/dto/register.dto.ts
+++ src/auth/dto/register.dto.ts
@@ -8,21 +8,25 @@ import {
 } from 'class-validator';
 
 export class RegisterDto {
-      @IsNotEmpty({message: "El nombre es obligatorio"})
-    @IsString({message: "El nombre debe ser texto"})
-    @MaxLength(50,{message: "El nombre debe tener como maximo 50 caracteres"})
+  @IsNotEmpty({ message: 'El nombre es obligatorio' })
+  @IsString({ message: 'El nombre debe ser texto' })
+  @MaxLength(50, { message: 'El nombre debe tener como maximo 50 caracteres' })
   nombre: string;
 
-  @IsNotEmpty({message: "El apellido es obligatorio"})
-    @IsString({message: "El apellido debe ser texto"})
-    @MaxLength(20,{message: "El apellido debe tener como maximo 20 caracteres"})
+  @IsNotEmpty({ message: 'El apellido es obligatorio' })
+  @IsString({ message: 'El apellido debe ser texto' })
+  @MaxLength(20, {
+    message: 'El apellido debe tener como maximo 20 caracteres',
+  })
   apellido: string;
 
-      @IsNotEmpty({message: "El email es obligatorio"})
-    @IsEmail({}, {message: "El email debe ser un correo electronico valid"})
-    @MinLength(3,{message: "El email debe tener como minimo 3 caracteres"})
-    @MaxLength(20,{message: "El email debe tener como maximo 20 caracteres"})
-    @Matches(/^[a-zA-Z0-9_]+$/, { message: "El email solo puede contener letras, números y guiones bajos" })
+  @IsNotEmpty({ message: 'El email es obligatorio' })
+  @IsEmail({}, { message: 'El email debe ser un correo electronico valid' })
+  @MinLength(3, { message: 'El email debe tener como minimo 3 caracteres' })
+  @MaxLength(20, { message: 'El email debe tener como maximo 20 caracteres' })
+  @Matches(/^[a-zA-Z0-9_]+$/, {
+    message: 'El email solo puede contener letras, números y guiones bajos',
+  })
   correo: string;
 
   @IsNotEmpty({ message: 'El nombre de usuario es obligatorio' })
@@ -39,11 +43,16 @@ export class RegisterDto {
   })
   nombreUsuario: string;
 
-    @IsNotEmpty({message: "La contraseña es obligatoria"})
-    @IsString({message: "La contraseña debe ser texto"})
-    @MinLength(6,{message: "La contraseña debe tener como minimo 6 caracteres"})
-    @Matches(/^(?=.*[A-Z])(?=.*\d)[a-zA-Z0-9_]{8,}$/, { message: "La contraseña debe contener al menos 8 caracteres, una mayúscula y un número." })
-    password: string;
+  @IsNotEmpty({ message: 'La contraseña es obligatoria' })
+  @IsString({ message: 'La contraseña debe ser texto' })
+  @MinLength(6, {
+    message: 'La contraseña debe tener como minimo 6 caracteres',
+  })
+  @Matches(/^(?=.*[A-Z])(?=.*\d)[a-zA-Z0-9_]{8,}$/, {
+    message:
+      'La contraseña debe contener al menos 8 caracteres, una mayúscula y un número.',
+  })
+  password: string;
 
   @IsNotEmpty()
   descripcion: string;
